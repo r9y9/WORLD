22c2b4a9b49ca0573909a1b27fec8bc77bf7a902
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2014-11-29 21:52:49 +0900

    remove unnecessary codes

--- world.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <world/star.h>
-
-int main() {
-  int a = GetFFTSizeForStar(44100);
-  printf("Hello world\n");
-  printf("%d\n", a);
-  return 0;
-}
--- world.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <world/star.h>
-
-int main() { 
-  int a = GetFFTSizeForStar(44100);
-  std::cout << "Hello world" << std::endl
-	    << a << std::endl; 
-  return 0;
-}
