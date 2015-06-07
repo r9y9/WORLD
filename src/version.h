ab4bc327c6ee3ba45268230d0027707ab9ba9778
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-06-07 13:27:13 +0900

    Add version.h/cpp

---
+++ src/version.h
@@ -0,0 +1,20 @@
+//-----------------------------------------------------------------------------
+// Ryuichi YAMAMOTO (zryuichi@gmail.com)
+//-----------------------------------------------------------------------------
+
+#ifndef WORLD_VERSION_H_
+#define WORLD_VERSION_H_
+
+#include "./dllexport.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+DLLEXPORT const char* GetWORLDVersion(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // WORLD_VERSION_H_
